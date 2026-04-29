114591c6daeb00f26986fee67538c23189fe4513
Pragati <btech15110.22@bitmesra.ac.in>
2026-04-29 23:17:14 +0530

    Added root endpoint

--- config/urls.py
+++ config/urls.py
@@ -5,3 +5,26 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("payouts.urls")),
 ]
+
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "message": "Payout Engine API is live",
+        "endpoints": [
+            "/api/v1/dashboard",
+            "/api/v1/payouts/history",
+            "/api/v1/ledger"
+        ]
+    })
+
+
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('', home),   # 👈 ADD THIS
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('payouts.urls')),
+]
+
